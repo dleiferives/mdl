5b7ff97ae7cbe91dae3e252d88adf2794c253e3a
Dylan Leifer-Ives <dleiferives@gmail.com>
2025-11-25 13:12:51 -0800

    Adding base structure

--- main.v
+++ main.v
@@ -3,8 +3,40 @@ mut:
 	id int
 }
 
-mut pratt := Pratt{
-	id: 1
+enum TokenKind {
+	EOF
+	IDENT
+	EQ
+	PLUS
+	LPAREN
+	RPAREN
 }
 
-println(pratt)
+struct Token {
+	kind TokenKind
+	str  string
+	pos  int
+}
+
+struct Lexer {
+	src string
+mut:
+	index int
+}
+
+fn is_alpha(c rune) bool {
+	return (c >= `a` && c <= `z`) || (c >= `A` && c <= `Z`) || c == `_`
+}
+
+fn is_digit(c rune) bool {
+	return c >= `0` && c <= `9`
+}
+
+mut test := 'a = 22'
+mut thing := match true {
+	is_alpha(test[0]) { 'ALPHA' }
+	else { 'else' }
+}
+
+println(test)
+print(thing)

3f8f58552d5dcf64cbe1ed5c211835f0fe19b84e
Dylan Leifer-Ives <dleiferives@gmail.com>
2025-11-30 18:58:26 -0800

    working graph thing

--- ir_namespace_graph.v
+++ ir_namespace_graph.v
@@ -11,6 +11,7 @@ pub mut:
 	children    []NSNodeId
 	linked      []NSNodeId
 	linked_name []string
+	valid       bool = true
 }
 
 @[heap]
@@ -73,12 +74,13 @@ pub fn (mut s NSSolver) add_children_tasks(block Block, parent_path string, id N
 	return result
 }
 
-pub fn (mut s NSSolver) add_linked_tasks(block Block, parent_path string, id NSNodeId) []NSNodeId {
+pub fn (mut s NSSolver) add_linked_tasks(block Block, parent_path string, id NSNodeId) ([]NSNodeId, []string) {
 	path := parent_path
 	if os.real_path(path) != path {
 		panic('could not get node name of ${path} as it was not absolute. all calls must be absolute')
 	}
 	mut result := []NSNodeId{}
+	mut nresult := []string{}
 	mut tasks := NSLinkTask{
 		parent:      id
 		parent_path: parent_path
@@ -91,10 +93,11 @@ pub fn (mut s NSSolver) add_linked_tasks(block Block, parent_path string, id NSN
 			tasks.list << stmt
 			tasks.ids << cid
 			result << cid
+			nresult << stmt.name.name
 		}
 	}
 	s.tasks.push(NSTask(tasks))
-	return result
+	return result, nresult
 }
 
 // path must be absolute
@@ -138,42 +141,70 @@ pub fn (mut s NSSolver) solve_child(child NamespaceDefinition, path string, id N
 	s.nodes[id].name = child.name.name
 	// Resolve what the node we're going to create will be named
 	block := child.block
-	s.nodes[id].children = s.add_children_tasks(block, p, id)
-	s.nodes[id].linked = s.add_linked_tasks(block, p, id)
+	s.nodes[id].children << s.add_children_tasks(block, p, id)
+	linked, names := s.add_linked_tasks(block, p, id)
+	s.nodes[id].linked << linked
+	s.nodes[id].linked_name << names
 	s.solved[id] = true
 	return id
 }
 
 pub fn (mut s NSSolver) solve_file(path string) NSNodeId {
+	println('solving file ${path}')
 	p := os.real_path(path)
 	mut id := s.nmap[p] or { s.new_node() }
+	println('working on node ${id}')
 	if s.solved[id] {
 		return s.nmap[p] or { panic('${p} should be solved and therefore in the map...') }
 	}
 	s.nodes[id].name = get_file_node_name(p)
+	println('nodes name is ${s.nodes[id].name}')
 	s.nmap[p] = id
 	// Resolve what the node we're going to create will be named
 	block := get_file_node_block(p)
-	s.nodes[id].children = s.add_children_tasks(block, p, id)
-	s.nodes[id].linked = s.add_linked_tasks(block, p, id)
+	s.nodes[id].children << s.add_children_tasks(block, p, id)
+	linked, names := s.add_linked_tasks(block, p, id)
+	s.nodes[id].linked << linked
+	s.nodes[id].linked_name << names
 	s.solved[id] = true
 	return id
 }
 
 pub fn (mut s NSSolver) solve(path string) ! {
+	println('starting solve of ${path}')
 	wd := os.getwd()
 	s.solve_file(path)
 	for !s.tasks.is_empty() {
+		println('solving task')
 		task := s.tasks.pop()!
 		match task {
 			NSLinkTask {
 				for idx, link in task.list {
 					id := task.ids[idx]
 					s.nodes[id].name = link.name.name
-					tpath := link.path.value
-					os.chdir(task.parent_path)!
-					println('tpath is ${tpath}')
+					println('task nodes name is ${s.nodes[id].name}')
+					tpath := link.path.value[1..link.path.value.len - 1]
+					println('task path is ${tpath}')
+					println('parent is ${task.parent_path}')
+					pp, _, _ := os.split_path(task.parent_path)
+					os.chdir(pp)!
 					l_path := os.real_path(tpath)
+					nid := s.nmap[l_path] or { id }
+					s.nmap[l_path] = nid
+					if nid != id {
+						println('${nid} != ${id}')
+						for lid, lv in s.nodes[task.parent].linked {
+							if lv == id {
+								s.nodes[task.parent].linked[lid] = nid
+							}
+						}
+						assert s.solved[id] == false
+						s.nodes[id].valid = false
+					}
+					if s.solved[nid] {
+						println('${nid} already solved')
+						continue
+					}
 					s.solve_file(l_path)
 				}
 			}
